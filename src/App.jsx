1db49e68fe3c247af268305cb0b1c1ea1e470f43
WataNegreirosMonteiro <wata.negreiros@estudante.ifro.edu.br>
2023-12-19 17:06:44 -0200

    refactor-created_response_notify

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,46 @@
+import { useState }  from "react";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Banner from "./components/molecules/Banner.jsx";
-import {Header} from "./components/organisms/Header.jsx";
+import { Header } from "./components/organisms/Header.jsx";
 import Footer from "./components/templates/Footer.jsx";
 import ContentTab from "./components/organisms/ContentTab.jsx";
-import {useState} from "react";
 import api from "../localizador_de_cep/src/services/api.js";
 
 export default function App() {
-    const [data, setData] = useState(false);
+    const [data, setData] = useState({});
+    const notify = (msg) => {
+        toast.error(msg, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+        })
+    }
 
     const handleData = async (cep) => {
         try {
-            const response = await api.get(`${cep}/json/`);
+            const response = await api.get(`${cep}/json/`)
             setData(response.data);
+
+            data.erro && notify('CEP não encontrado ou inválido.')
         } catch (error) {
             console.error('Erro ao buscar CEP:', error);
-            alert('CEP não encontrado ou ocorreu um erro na busca.');
+            notify('CEP não encontrado ou ocorreu um erro na busca.')
         }
     }
 
     return (
         <main className="w-full flex justify-center flex-col">
-            <Banner description='Saiba mais e veje mais projetos' buttonText='LinkedIn'/>
-            <Header handleSearch={ handleData }/>
-            { data && <ContentTab data={data}/>}
-            <Footer/>
+            <Banner description='Saiba mais e veje mais projetos' buttonText='LinkedIn' />
+            <Header handleSearch={handleData} />
+            { data.cep && <ContentTab data={data} /> }
+            <Footer />
         </main>
     )
 }
